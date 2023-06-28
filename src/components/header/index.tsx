a4ca8c59c2603d9474328af8214c47fa01402921
denervalspelizario <denervalspelizario@gmail.com>
2023-06-28 10:50:36 -0300

    update appCotacaoMoeda criacaod e component layout que renderiza todos o corpo da pagina, e criacao de header ja estilizado

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import styles from './header.module.css'; // importando o estilo
+import logoImg from '../../assets/logo.svg';
+
+
+
+export default function Header(){
+  return(
+    <header className={styles.container}>
+      <div className={styles.logo}>
+        
+        <Link to='/'>
+          <img src={logoImg} alt="Logo Cripto" />  {/* Ao clicar na img vai pra home */} 
+        </Link>
+        
+      </div>
+    </header>
+  )
+}
